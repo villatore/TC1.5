8cf9aa65dcebd524b4089f4688c38043545a3f60
villatore <villatore@yahoo.com.mx>
2014-01-28 08:27:05 -0600

    conexión index.js

--- www/js/index.js
+++ www/js/index.js
@@ -41,8 +41,23 @@ var app = {
                 } else {
                     console.log("Su dispositivo no cuenta con internet");
                 }
+
+            var networkState = navigator.connection.type;
+
+            var states = {};
+            states[Connection.UNKNOWN]  = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI]     = 'WiFi connection';
+            states[Connection.CELL_2G]  = 'Cell 2G connection';
+            states[Connection.CELL_3G]  = 'Cell 3G connection';
+            states[Connection.CELL_4G]  = 'Cell 4G connection';
+            states[Connection.CELL]     = 'Cell generic connection';
+            states[Connection.NONE]     = 'No network connection';
+
+            alert('Connection type: ' + states[networkState]);
+
 			console.log("Se cambiará a la siguiente pag");
-            window.location = "index2.html";
+            //window.location = "index2.html";
 			console.log("Se cambia a la siguiente pag");
 	},
     // Update DOM on a Received Event
